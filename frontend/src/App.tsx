c6e645b6f849ada07a71293bc4ffedfe0c9d9ae1
griminir <viktor.elliot.king@gmail.com>
2024-09-23 11:35:22 +0200

    added grid layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Button colorScheme='blue'>Button</Button>
+      <Grid
+        templateAreas={`"header header header" "search search search" "main main main"`}
+      >
+        <GridItem area='header' bg='green' color='white'>
+          onions & garlic
+        </GridItem>
+        <GridItem area='search' bg='blue' color='white'>
+          search
+        </GridItem>
+        <GridItem area='main' bg='black' color='white'>
+          main
+        </GridItem>
+      </Grid>
     </>
   );
 }
