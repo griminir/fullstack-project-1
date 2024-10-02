5f6af4464c732443596080027ff6e0fbc51ae30c
griminir <viktor.elliot.king@gmail.com>
2024-10-02 14:37:50 +0200

    need to work on this later

--- frontend/src/components/RecipeHeading.tsx
+++ frontend/src/components/RecipeHeading.tsx
@@ -1,15 +1,15 @@
-import { Box, Heading, Text, Image } from '@chakra-ui/react';
-import useRecipes from '../hooks/useRecipes';
+// import { Box, Heading, Text, Image } from '@chakra-ui/react';
+// import useRecipes from '../hooks/useRecipes';
 
-const RecipeHeading = () => {
-  const {data, error, isLoading} = useRecipes();
-  return (
-    <Box>
-      <Image src={data.picture} />
-      <Heading fontSize='2xl'>{recipe.title}</Heading>
-      <Text>{recipe.description}</Text>
-    </Box>
-  );
-};
+// const RecipeHeading = () => {
+//   const {data, error, isLoading} = useRecipes();
+//   return (
+//     <Box>
+//       <Image src={data.picture} />
+//       <Heading fontSize='2xl'>{recipe.title}</Heading>
+//       <Text>{recipe.description}</Text>
+//     </Box>
+//   );
+// };
 
-export default RecipeHeading;
+// export default RecipeHeading;
