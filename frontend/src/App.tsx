56a86b8993e27b4f02cfd09cdba9bef35d0245d4
griminir <viktor.elliot.king@gmail.com>
2024-10-03 13:33:17 +0200

    messing around with layouts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css';
-import MainPageTemplate from './components/MainPageTemplate';
-import RecipePageTemplate from './components/RecipePageTemplate';
+// import MainPageTemplate from './components/MainPageTemplate';
+import NavBar from './components/NavBar';
+import RecipeGrid from './components/RecipeGrid';
+// import RecipePageTemplate from './components/RecipePageTemplate';
+import { Button, Grid, GridItem, HStack } from '@chakra-ui/react';
 
 function App() {
   return (
     <>
-      <MainPageTemplate />
-      <RecipePageTemplate />
+      <Grid templateAreas={`"header" "main"`}>
+        <GridItem area='header' bg='green' color='white'>
+          <NavBar />
+          <HStack justifyContent={'center'}>
+            <Button colorScheme='teal'>Add new recipe</Button>
+          </HStack>
+        </GridItem>
+        <GridItem area='main'>
+          <RecipeGrid />
+        </GridItem>
+      </Grid>
     </>
   );
 }
