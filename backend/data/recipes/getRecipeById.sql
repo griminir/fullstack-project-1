7ea0a83c7243f6fc5e12650af8962e56759c1a93
griminir <viktor.elliot.king@gmail.com>
2024-10-03 08:58:47 +0200

    changed db structure to help with api flow

--- backend/data/recipes/getRecipeById.sql
+++ backend/data/recipes/getRecipeById.sql
@@ -1,13 +1,2 @@
-
-select r.id,
-r.title,
-[r].[description],
-r.picture,
-ing.id as ingredientId,
-ing.quantity,
-ing.unit,
-[ing].[name]
-from [dbo].[recipes] as r
-inner join ingredients as ing on
-ing.recipeId = r.id
-where r.id=@id
+select [id], [title], [description], [picture] from [dbo].[recipes]
+where id=@id
